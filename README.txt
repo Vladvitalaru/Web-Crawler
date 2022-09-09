094c3c8197602ea59bec904a66f202437c2ba29c
Vlad <vladvitalaru@yahoo.com>
2022-09-08 18:39:19 -0700

    Implemented Json Adjacency Matrix

---
+++ README.txt
@@ -0,0 +1,17 @@
+Vlad Vitalaru 
+CS454
+
+The script is ran using command: python3 webcrawler.py
+
+
+A prompt will ask for a url input by the user followed by how many pages to scrape in the terminal.
+
+Paste a link to an HTML site as the starting seed.
+
+Ex: https://escapefromtarkov.fandom.com/wiki/Escape_from_Tarkov_Wiki
+
+
+The program will begin scraping through the url, downloading page text content to a "Data" folder at the path where the script was ran from. It will also create a json at the script's path, storing an adjacency matrix of links.
+
+
+Please refer to Latex Writeup for further details on implementation.
--- webcrawler.py
+++ webcrawler.py
@@ -1,3 +1,7 @@
+#Vlad Vitalaru
+#CS 454 Fall 2022
+#Assignment 1
+
 import requests
 from bs4 import BeautifulSoup
 from collections import deque
@@ -5,37 +9,29 @@ from urllib import parse
 from urllib import robotparser
 import socket
 import os
+import json
 
-'''webCrawler class, purpose is to obtain a link from a user and being downloading the pages
+'''webCrawler class, purpose is to obtain a link from a user and download the page
    along with the linked pages in a breadth first manner    
 '''
 class webCrawler:
     def __init__(self, pages):
         self.pages = pages          #The limit of pages to crawl
-        self.count = 0              #How many pages have we crawled
+        self.count = 0              #How many pages we've crawled
         self.que = deque([])        #Que that allows us to crawl in a breadth first manner
         self.visited = set([])      #Set holding all visited links
         self.robotsDict = {}        #Dict keeping track of what robots.txt files we have used
-        
-        self.seed = 'https://wsu.edu/'
-        self.seed2 = "https://wsu.edu/about/facts/"
-    
+        self.matrixDict = {}        #Dict used for an adjacency matrix of links
+ 
     '''Method that obtains the initial seed and calls the main loop of program'''
     def getSeed(self):
-        
+        print("\nScript crawls URL's and downloads text files into 'Data' folder")
         print("\nEnter a starting URL seed to begin scraping data:")
-       # seed = str(input().strip())
-       # print(seed)
-        seed = "https://wsu.edu/"
-       # seed =  "https://research.wsu.edu/documents/2019/02/2017-annual-report.pdf/"
-        seed1 = "https://twitter.com/WSUPullman"
-        seed3 = "https://s3.wp.wsu.edu/uploads/sites/625/2022/02/nwccu-logo.png"
-        seed2 = "https://fafsa.ed.gov/"
-        seed4 = "https://www.usnews.com/education/best-high-schools/washington/districts/pullman-school-district/pullman-high-school-21102"
-        seed5 = "https://foundation.wsu.edu/give/?fund=6754ebb1-2dbf-4073-86e8-67c7e36b0e59&cat=idonate_programs&area=idonate_office_of_research_and_graduate_school&utm_source=office-of-research-and-graduate-school&utm_medium=wsu-link&utm_campaign=research-program-development-fund"
+        seed = str(input().strip())
+        print()
         
         parsed = parse.urlsplit(seed)
-        if parsed.hostname is not None:
+        if parsed.hostname is not None:                                                     #If the hostname is not None
             robots = parse.urljoin(parsed.scheme + '://' + parsed.hostname, "robots.txt")   #Parse URL and append robots.txt    
             Crawl = robotparser.RobotFileParser()
             Crawl.set_url(robots)
@@ -44,87 +40,89 @@ class webCrawler:
                 Crawl.read()
             except:
                 print("Link is not accessible!")
+                print("Please try another link!\n")
                 exit(1)
             if Crawl.can_fetch('*', seed):          #Check if the initial link can be crawled
                 try:
-                    status = requests.get(seed, timeout=2)
-                except:
-                    print("Not a valid link!")
+                    status = requests.get(seed, timeout=2)   #Try a get request on initial seed
+                except:   
+                    print("Invalid link!")
+                    print("Please try another link!\n")
                     exit(1)
                 else:
-                    if status.status_code != 404:
-                        self.que.append(seed)
-                        self.visited.add(seed) 
-                        self.crawlingLoop()         #Begin main loop of crawling
-                        print("---end of loop---")
-                        return
+                    html = status.headers.get('Content-Type')
+                    if html is not None:
+                        if "text/html" in html:
+                            self.que.append(seed)
+                            self.visited.add(seed) 
+                            self.crawlingLoop()         #Begin main loop of crawling
+                            print("Finished crawling for data!")
+                            return
         else:
-            print("Error establishing connection")
-            print("Please try another link!")
+            print("\nInvalid link!")
+            print("Please try another link!\n")
             exit(1)
     
     
     '''Main crawling loop where links are popped and pushed on the que with all their links'''
     def crawlingLoop(self):
-        newDomain = False
+        newDomain = False                                    
         while self.count < self.pages:                       #Scrape while the pages is less than the limit
             currentPage = self.que.popleft()                 #Pop off the deck for the next link
-            print(currentPage)
             parsed = parse.urlsplit(currentPage)
             if parsed.hostname is not None:                          #If valid hostname returned
                 robotCheck = self.robotsDict.get(parsed.hostname)    #Try to find the robots page in the dictionary
                 if robotCheck is not None:
-                    if robotCheck.can_fetch("*", currentPage):       #If we have domain before and 
-                        Crawl = robotCheck
+                    if robotCheck.can_fetch("*", currentPage):       #If we have domain before and can fetch
+                        Crawl = robotCheck                           #Set Crawl to the robot.txt object obtained from dictionary
                         newDomain = False
-                       # print("seenbefore")
                 else:
-                   # print("newdomain")
-                    Crawl = robotparser.RobotFileParser()
+                    Crawl = robotparser.RobotFileParser()                                           #Create a new robotparser if new domain
                     robots = parse.urljoin(parsed.scheme + '://' + parsed.hostname, "robots.txt")   #Parse URL and append robots.txt
                     Crawl.set_url(robots)
                     newDomain = True
                 try:
                     Crawl.read()
                 except:
-                    print("failed")
                     continue
                 else:
-                    if Crawl.can_fetch('*', currentPage):
+                    if Crawl.can_fetch('*', currentPage):       #If page can be crawled
                         try: 
-                            status = requests.get(currentPage, timeout = 2)
+                            status = requests.get(currentPage, timeout = 2)     #Try a get request
                         except:
-                            print("failed to fetch content")
                             continue
                         else:
-                            #html = requests.head(currentPage, timeout = 2)
-                            html = status.headers.get('Content-Type')
+                            html = status.headers.get('Content-Type')          #obtain the Content-Type and check if its text/html
                             if html is not None:
                                 if "text/html" in html:
-                                    if newDomain == True:    #If domain has not been seen before, store the robots object in dictionary
-                                        self.robotsDict[parsed.hostname] = Crawl
-                                    self.grabLinks(currentPage, status.text)        #Pass the text from the get object           
+                                    if newDomain == True:  
+                                        self.robotsDict[parsed.hostname] = Crawl    #If domain has not been seen before, store the robots object in dictionary
                                     self.visited.add(currentPage)
+                                    self.grabLinks(currentPage, status.text)        #Pass the text from the get object                                               
                                     self.count+=1       #increment how many pages weve crawled
-                                    print(self.count)
+                                    print(f"{self.count} Crawling: {currentPage}")
                                 
         #------End of While---- 
             
             
     '''Method which takes a page, and scrapes all the links, adding them to a que and visited set'''
-    def grabLinks(self, currentPage, object):
-
-        Soup = BeautifulSoup(object, 'lxml')
+    def grabLinks(self, currentPage, Text):
+        self.matrixDict[currentPage] = []    #Add current page to adjacency matrix
+        Soup = BeautifulSoup(Text, 'lxml')
         for link in Soup.find_all('a'):         #Grab all the links from page
             href = link.get('href')
             if href is not None and href.startswith("htt" or "/"):
                 if href[0] == "/":                      #Check for relative links
                     href = currentPage + href           #Prefix current link                  
                 if href not in self.visited:
+                    self.visited.add(href)
                     self.que.append(href)               #Add links to que
+                    self.matrixDict[currentPage].append(href)   #Add link to adjacency matrix
                     
-        self.download(currentPage, object)      #Download page content before moving onto next one in que
-        
+        path = os.path.dirname(__file__) 
+        with open(f"{path}/JsonMatrix.json", 'w') as file: #Create an adjacency matrix file using json dump
+            json.dump(self.matrixDict, file)
+        self.download(currentPage, Text)      #Download page content before moving onto next one in que
         return
     
     
@@ -136,25 +134,25 @@ class webCrawler:
         path = os.path.dirname(__file__)
         path = os.path.join(path, folder)
         os.makedirs(path, mode=0o777, exist_ok=True)    #Create a directory "Pages" where the script is located
-        
         Soup = BeautifulSoup(object, 'lxml')
         content = Soup.get_text(" ", strip=True)    #Grab all the text content on the page
         
-        url = url.replace('/','').replace(':','')   #Clean up URL for file naming conventions
-        
-        if len(url) > 255:
-            url = hash(url)            
+        name = url.replace('/','').replace(':','')   #Clean up URL for file naming conventions
+        backup = name + "\n" 
+        if len(url) > 255:             #If file is too long, hash it before naming it
+            name = str(hash(url))
             
-        with open(f'{path}/{url}.txt', 'w') as f:   #Create file and write page contents
-            f.write(content)
-        return
+        with open(f'{path}/{name}.txt', 'w') as f:   #Create file and write page contents
+            if name.startswith("h") is False:        #If name is a hash, write full link to the first line of file
+                f.write(backup)
+            f.write(content)         #Write text content to file
+        return      
 
 
 def main():
     socket.setdefaulttimeout(2)  #Used to avoid hanging for too long on sites
     crawler = webCrawler(5000)
-    crawler.getSeed()
-   # crawler.download("https://foundation.wsu.edu/give/?fund=6754ebb1-2dbf-4073-86e8-67c7e36b0e59&cat=idonate_programs&area=idonate_office_of_research_and_graduate_school&utm_source=office-of-research-and-graduate-school&utm_medium=wsu-link&utm_campaign=research-program-development-fund")
-
+    crawler.getSeed()           #Start the program cycle by obtaining initial seed from user
+    
 if __name__ == '__main__':
 	main()
